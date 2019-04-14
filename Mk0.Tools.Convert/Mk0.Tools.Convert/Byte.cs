30e2ff6366e0b0ce326af827c1cc9cfb3cc2c136
Manuel Kamper <manuel@kmpr.at>
2019-04-14 11:00:17 +0200

    compatibility

--- Mk0.Tools.Convert/Mk0.Tools.Convert/Byte.cs
+++ Mk0.Tools.Convert/Mk0.Tools.Convert/Byte.cs
@@ -2,9 +2,9 @@
 
 namespace Mk0.Tools.Convert
 {
-    public static class Byte
+    public static class ByteConvert
     {
-        public static System.Drawing.Image ToImage(byte[] byteArrayIn)
+        public static System.Drawing.Image ToImage(this byte[] byteArrayIn)
         {
             MemoryStream ms = new MemoryStream(byteArrayIn);
             System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
--- Mk0.Tools.Convert/Mk0.Tools.Convert/Image.cs
+++ Mk0.Tools.Convert/Mk0.Tools.Convert/Image.cs
@@ -2,9 +2,9 @@
 
 namespace Mk0.Tools.Convert
 {
-    public static class Image
+    public static class ImageConvert
     {
-        public static byte[] ToByte(System.Drawing.Image imageIn)
+        public static byte[] ToByte(this System.Drawing.Image imageIn)
         {
             MemoryStream ms = new MemoryStream();
             imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
