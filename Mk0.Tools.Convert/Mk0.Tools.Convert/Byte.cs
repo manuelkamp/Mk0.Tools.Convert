94e1323d7a72fa79c9781f55ca2d48a85c536722
Manuel Kamper <manuel@kmpr.at>
2019-04-14 10:00:38 +0200

    initial

---
+++ Mk0.Tools.Convert/Mk0.Tools.Convert/Byte.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Mk0.Tools.Convert
+{
+    public static class Byte
+    {
+        public static System.Drawing.Image ToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
---
+++ Mk0.Tools.Convert/Mk0.Tools.Convert/Image.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Mk0.Tools.Convert
+{
+    public static class Image
+    {
+        public static byte[] ToByte(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+    }
+}
