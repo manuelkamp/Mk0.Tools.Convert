2fbab2c9ad1da2adef4e2f05b400427e8c32d37c
Manuel Kamper <manuelk@outlook.com>
2019-05-04 14:45:28 +0200

    redesign byte to image

--- Mk0.Tools.Convert/Byte.cs
+++ Mk0.Tools.Convert/Byte.cs
@@ -9,7 +9,11 @@ namespace Mk0.Tools.Convert
         {
             using (MemoryStream ms = new MemoryStream(byteArray))
             {
-                return Image.FromStream(ms);
+                using (var bitmap = new Bitmap(ms))
+                {
+                    var bitmapCopy = new Bitmap(bitmap, bitmap.Width, bitmap.Height);
+                    return bitmapCopy;
+                }
             }
         }
     }
